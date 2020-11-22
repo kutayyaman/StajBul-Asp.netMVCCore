7d14e7bade9fd2d83c74d2a393209add955f4f54
Kutay Yaman <yamankutay1@gmail.com>
2020-11-22 17:07:42 +0300

    custom property validation added.(ornek olmasi acisindan MustBeTrueAttribute property validation'i olusturdum bunu birisi uye olurken mesela TermsAccepted alanina uygularim eger kabul etmediyse geriye validasyon mesaji dondurur ve ekran gosteririm onu yani kisacasi kendi property validation'larimizi olusturmak istersek buradan bakabiliriz.)

---
+++ StajBul.WebUI/Infrastructure/MustBeTrueAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StajBul.WebUI.Infrastructure
+{
+    public class MustBeTrueAttribute : Attribute, IModelValidator //Bunu artik [MustBeTrueAttribute(errorMessage = "istediginiz bir mesaj yazabiirsin")] olarak kullanabilirim.
+    {
+        public bool isRequired => true;
+
+        public string ErrorMessage { get; set; } = "Kullanım Koşullarını Kabul Etmelisiniz.";
+
+        public IEnumerable<ModelValidationResult> Validate(ModelValidationContext context)
+        {
+            var value = context.Model as bool?;
+
+            if(!value.HasValue || value.Value == false)
+            {
+                return new List<ModelValidationResult> { 
+                    new ModelValidationResult("", ErrorMessage)
+                };
+            }
+            else
+            {
+                return Enumerable.Empty<ModelValidationResult>();
+            }
+        }
+    }
+}
